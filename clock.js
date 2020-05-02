10f4d8b5213b48d505b4b9936d8f86a46a0f4db5
md2eoseo <md2eoseo@gmail.com>
2020-05-02 12:35:28 +0200

    update only when seconds is '0'

--- clock.js
+++ clock.js
@@ -1,14 +1,19 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
+let initialTime = true;
 
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+  if (seconds == 0 || initialTime) {
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+      minutes < 10 ? `0${minutes}` : minutes
+    }`;
+    initialTime = false;
+  }
 }
 
 function init() {
